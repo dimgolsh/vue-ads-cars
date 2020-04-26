a2cf875af33dc0c90d0b2c7c178036655bd27a9b
dimon <dimagol64@gmail.com>
2020-04-26 12:06:06 +0300

    ad list

--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,8 +1,36 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>AdList</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">My ads</h1>
+
+        <v-card
+          class="elevation-10 mb-3"
+          v-for="ad in ads"
+          :key="ad.id"
+        >
+          <v-layout row>
+            <v-flex xs4>
+              <v-img
+                :src="ad.imageSrc"
+                height="160px"
+              ></v-img>
+            </v-flex>
+            <v-flex xs8>
+              <v-card-text>
+                <h2 class="text--primary">{{ad.title}}</h2>
+                <p>{{ad.description}}</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  class="info"
+                  :to="'/ad/' + ad.id"
+                >Open</v-btn>
+              </v-card-actions>
+            </v-flex>
+          </v-layout>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,7 +39,24 @@
 <script>
   export default {
     data () {
-      return {}
+      return {
+        ads: [
+          {
+            title: 'First ad',
+            description: 'Hello i am description',
+            promo: false,
+            imageSrc: 'https://images.unsplash.com/photo-1494905998402-395d579af36f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+            id: '123'
+          },
+          {
+            title: 'Second ad',
+            description: 'Hello i am description',
+            promo: true,
+            imageSrc: 'https://images.unsplash.com/photo-1494905998402-395d579af36f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+            id: '1234'
+          }
+        ]
+      }
     }
   }
 </script>
