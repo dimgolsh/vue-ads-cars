68dc3b780bfbe8381d84aad2051289635e0260be
dimon <dimagol64@gmail.com>
2020-04-26 17:37:47 +0300

    order/ color

--- src/main.js
+++ src/main.js
@@ -6,7 +6,19 @@ import "vuetify/dist/vuetify.min.css";
 import store from "./components/store";
 import * as fb from "firebase";
 import BuyModalComponent from '@/components/Shared/BuyModal'
-Vue.use(Vuetify);
+// import colors from 'vuetify/es5/util/colors'
+
+Vue.use(Vuetify, {
+  // theme: {
+  //   primary: "#f44336",
+  //   secondary: "#9575CD",
+  //   accent: "#9c27b0",
+  //   error: "#f44336",
+  //   warning: "#3949AB",
+  //   info: "#2196f3",
+  //   success: "#4caf50"
+  // }
+})
 
 
 Vue.component('app-buy-modal', BuyModalComponent)
